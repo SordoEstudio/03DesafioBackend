29fd8d0c88cf10dec935b2ba7a07b28826b62edc
Pablo <pabloalbera23@gmail.com>
2024-06-09 19:15:23 -0300

    thumbnail por thumbnails

--- src/daos/mongodb/models/productModel.js
+++ src/daos/mongodb/models/productModel.js
@@ -6,7 +6,7 @@ const productSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  thumbnail: { type: Array },
+  thumbnails: { type: Array },
   code: { type: String, required: true },
   stock: { type: Number, required: true },
   category: { type: String, required: true },
